cc7b4c1c36a8692b1c10c29defe0a5376241391b
Dan Gilleland <dgilleland@nait.ca>
2024-10-04 02:47:38 -0600

    Fixed test data - was missing birthdates

--- DDL/SchoolTranscriptTestData.sql
+++ DDL/SchoolTranscriptTestData.sql
@@ -9,7 +9,7 @@ SELECT DB_NAME() AS 'Initial Database'  -- should be [master] when first execute
 GO
 -- We can change the context through the USE statement,ensuring that we're inside the correct database
 USE [SchoolTranscript]  -- Switch to the desired database
-GO
+GO  -- The GO keyword tells the database server to GO ahead and make these changes
 SELECT DB_NAME() AS 'Active Database'   -- shows that we are in the correct database
 GO
 
@@ -22,16 +22,16 @@ VALUES  ('Sara', 'Bellum', 'Aug 1, 2005', 1)
 
 -- Add multiple students (with default constraints)
 INSERT INTO Students(GivenName, Surname, DateOfBirth)
-VALUES  ('Oliver', 'Nerr', ''), -- O. Nerr
-        ('Stewart', 'Dent', ''), -- Stew Dent
-        ('Yu', 'Sur', ''),
-        ('Anne', 'Nother', ''),
-        ('Dee', 'Vellop-Ur', '')
+VALUES  ('Oliver', 'Nerr', 'April 1, 2001'), -- O. Nerr
+        ('Stewart', 'Dent', 'July 23, 2003'), -- Stew Dent
+        ('Yu', 'Sur', 'Feb 29, 2000'),
+        ('Anne', 'Other', 'Nov 3, 2004'),
+        ('Dee', 'Vellop-Ur', 'Dec 4, 1999')
 
 /*
 -- Add these students after altering the table constraints
 INSERT INTO Students(GivenName, MiddleNames, Surname, DateOfBirth)
-VALUES  ('Reginald', 'Ian', 'Stirr', '') -- Reg I. Stirr
+VALUES  ('Reginald', 'Ian', 'Stirr', 'Aug 1, 2005') -- Reg I. Stirr
 */
 
 -- Explore the results (Query statement)
@@ -41,5 +41,9 @@ SELECT * FROM Students
 /*
 -- Samples of "bad" data (violates CHECK constraints)
 INSERT INTO Students(GivenName, Surname, DateOfBirth, Enrolled)
-VALUES  ('B', '4', 'Aug 20, 2025', 1)
+VALUES  ('B', '4', 'Aug 20, 2025', 1) -- Bad GivenName and Surname
+
+INSERT INTO Students(GivenName, Surname, DateOfBirth)
+VALUES  ('Oh', 'No', 'Feb 5, 2045') -- This is a bad date
+
 */
